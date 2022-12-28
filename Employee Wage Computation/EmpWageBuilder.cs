4ddc7350eced988e58b212a77573f14659a0844b
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-28 13:47:42 +0530

    [Jyoti Rao] Add: Add Part Time Employee

--- Employee Wage Computation/EmpWageBuilder.cs
+++ Employee Wage Computation/EmpWageBuilder.cs
@@ -9,22 +9,25 @@ namespace Employee_Wage_Computation
         public static void CheckAttendance()
         {
             int Full_Time = 1;
+            int Part_Time = 2;
             int Emp_Per_Hours = 20;
-            int empHrs = 0, empWage;
+            int empHrs, empWage;
             Random random = new Random();
-            int check = random.Next(0, 2);
+            int check = random.Next(0, 3);
             if (check == Full_Time)
             {
                 empHrs = 8;
-                Console.WriteLine("Employee is Present");
+            }
+            else if (check == Part_Time)
+            {
+                empHrs = 4;
             }
             else
             {
                 empHrs = 0;
-                Console.WriteLine("Employee is Absent");
             }
             empWage = empHrs * Emp_Per_Hours;
-            Console.WriteLine("Daily Employee Wage : " + empWage);
+            Console.WriteLine("Employee Wage : " + empWage);
         }
     }
 }
