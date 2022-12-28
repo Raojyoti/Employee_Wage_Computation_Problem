254aeab0db143c75e7378cc97e59c66972e25db2
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-28 13:19:07 +0530

    [Jyoti Rao] Attendance: Check Employee Present or Absent

---
+++ Employee Wage Computation/EmpWageBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Computation
+{
+    public class EmpWageBuilder
+    {
+        public static void CheckAttendance()
+        {
+            int Full_Time = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ Employee Wage Computation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_Computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilder.CheckAttendance();
+        }
+    }
+}
