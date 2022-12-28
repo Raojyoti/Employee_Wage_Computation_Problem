6229c9fd67f721af8d8e0abeb3ce41c130d9d670
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-28 13:35:23 +0530

    [Jyoti Rao] DailyWage: Calculate Daily Employee Wage

--- Employee Wage Computation/EmpWageBuilder.cs
+++ Employee Wage Computation/EmpWageBuilder.cs
@@ -9,16 +9,22 @@ namespace Employee_Wage_Computation
         public static void CheckAttendance()
         {
             int Full_Time = 1;
+            int Emp_Per_Hours = 20;
+            int empHrs = 0, empWage;
             Random random = new Random();
             int check = random.Next(0, 2);
             if (check == Full_Time)
             {
+                empHrs = 8;
                 Console.WriteLine("Employee is Present");
             }
             else
             {
+                empHrs = 0;
                 Console.WriteLine("Employee is Absent");
             }
+            empWage = empHrs * Emp_Per_Hours;
+            Console.WriteLine("Daily Employee Wage : " + empWage);
         }
     }
 }
