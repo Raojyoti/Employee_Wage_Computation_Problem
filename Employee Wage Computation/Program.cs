3f231201e4dd2859e54862c23b85cc68ef8516ab
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-28 15:08:30 +0530

    [Jyoti Rao] MultipleCompanies: Compute Employee wage for multiple companies

--- Employee Wage Computation/Program.cs
+++ Employee Wage Computation/Program.cs
@@ -6,7 +6,8 @@ namespace Employee_Wage_Computation
     {
         static void Main(string[] args)
         {
-            EmpWageBuilder.computeEmpWage();
+            EmpWageBuilder.computeEmpWage("TCS", 20, 2, 10);
+            EmpWageBuilder.computeEmpWage("Capgemini", 20, 4, 10);
         }
     }
 }
